ea79141e859340c42d8ac7843b645f05e136bc54
ammar <ammarnazmi44@gmail.com>
2024-01-01 18:48:51 +0800

    admin-model

---
+++ app/Models/Admin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Database\Factories\AdminFactory;
+
+class Admin extends Model
+{
+    use HasFactory;
+}
